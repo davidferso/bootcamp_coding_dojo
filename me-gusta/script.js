9ad1d0c996f1ff76e8d07fe79c25355be00b29fb
davidferso <111031067+davidferso@users.noreply.github.com>
2022-12-02 21:57:16 -0300

    Create script.js

---
+++ me-gusta/script.js
@@ -0,0 +1,12 @@
+function like_neil_m() {
+    var number_likes_neil_m = document.querySelector("#number-likes-neil-m");
+    number_likes_neil_m.innerText++;
+}
+function like_nichole_k() {
+    var number_likes_nichole_k = document.querySelector("#number-likes-nichole-k");
+    number_likes_nichole_k.innerText++;
+}
+function like_jim_r() {
+    var number_likes_jim_r = document.querySelector("#number-likes-jim-r");
+    number_likes_jim_r.innerText++;
+}
